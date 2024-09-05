d126686ae451415796b64847f94a306ce2121b0a
cmckinney-59 <chazdmckinney@gmail.com>
2024-09-05 11:17:27 -0600

    added input roman box and output baybayin box

--- src/app/pages/baybayin-page/baybayin-page.component.ts
+++ src/app/pages/baybayin-page/baybayin-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-baybayin-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './baybayin-page.component.html',
   styleUrl: './baybayin-page.component.scss'
 })
 export class BaybayinPageComponent {
-
+  // Variable to store user input
+  userInput: string = '';
 }
