e0828a1ac24bbb0abc05aceab7b5aeae6e32340a
cmckinney-59 <chazdmckinney@gmail.com>
2024-10-09 12:51:29 -0600

    added j and qu functionality to modal

--- src/app/pages/baybayin-page/dialogs/word-review-dialog/word-review-dialog.component.ts
+++ src/app/pages/baybayin-page/dialogs/word-review-dialog/word-review-dialog.component.ts
@@ -13,8 +13,10 @@ import { NgIf } from '@angular/common';
 export class WordReviewDialogComponent {
   words: string[] = [];
   currentWordIndex: number = 0;
-  currentChIndex: number = 0;
   currentCIndex: number = 0;
+  currentChIndex: number = 0;
+  currentJIndex: number = 0
+  currentQuIndex: number = 0;
   updatedInput: string = '';
 
   constructor(
@@ -27,23 +29,53 @@ export class WordReviewDialogComponent {
     this.processCurrentWord();  // Start processing the first word
   }
 
+
   // Method to process the current word
   processCurrentWord(): void {
     if (this.currentWordIndex < this.words.length) {
       let currentWord = this.words[this.currentWordIndex];
-      this.currentChIndex = currentWord.indexOf('ch');  // Find the first occurrence of 'ch'
       this.currentCIndex = currentWord.indexOf('c');  // Find the first occurrence of 'c'
-      
-      if (this.currentChIndex !== -1) {
+      this.currentChIndex = currentWord.indexOf('ch');  // Find the first occurrence of 'ch'
+      this.currentJIndex = currentWord.indexOf('j');  // Find the first occurrence of 'j'
+      this.currentQuIndex = currentWord.indexOf('qu');  // Find the first occurrence of 'qu'
+
+      // Check for 'qu'
+      if (this.currentQuIndex !== -1) {
+        this.promptForQu();  // Ask for 'qu' replacement
+      } 
+      // Check for 'ch'
+      else if (this.currentChIndex !== -1) {
         this.promptForCh();  // Ask for 'ch' replacement
-      } else if (this.currentCIndex !== -1) {
+      } 
+      // Check for 'j'
+      else if (this.currentJIndex !== -1) {
+        this.promptForJ();  // Ask for 'j' replacement
+      } 
+      // Check for 'c' (that is not part of 'ch')
+      else if (this.currentCIndex !== -1 && this.currentChIndex === -1) {
         this.promptForC();  // Ask for 'c' replacement
-      } else {
-        this.nextWord();  // Move to the next word if no 'ch' or 'c'
+      } 
+      // No 'qu', 'ch', 'j', or 'c', move to next word
+      else {
+        this.nextWord();  // Move to the next word if no matches
       }
     } else {
       this.finish();  // Finish when all words are processed
     }
+  }
+
+  // Replace 'c' (but not 'ch') with 's'
+  replaceCWithS(): void {
+    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace(/c(?!h)/, 's');
+    this.updateInput();
+    this.processCurrentWord();  // Process again in case there are more 'c' occurrences
+  }
+
+  // Replace 'c' (but not 'ch') with 'k'
+  replaceCWithK(): void {
+    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace(/c(?!h)/, 'k');
+    this.updateInput();
+    this.processCurrentWord();  // Process again in case there are more 'c' occurrences
   }
 
   // Replace 'ch' with 'tiy'
@@ -60,18 +92,32 @@ export class WordReviewDialogComponent {
     this.processCurrentWord();  // Process again in case there are more 'ch' occurrences
   }
 
-  // Replace 'c' (but not 'ch') with 's'
-  replaceCWithS(): void {
-    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace(/c(?!h)/, 's');
+  // Replace 'j' with 'diy'
+  replaceJWithDiy(): void {
+    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace('j', 'diy');
     this.updateInput();
-    this.processCurrentWord();  // Process again in case there are more 'c' occurrences
+    this.processCurrentWord();  // Process again in case there are more 'j' occurrences
+  }
+  
+  // Replace 'ch' with 'k'
+  replaceJWithH(): void {
+    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace('j', 'h');
+    this.updateInput();
+    this.processCurrentWord();  // Process again in case there are more 'j' occurrences
   }
 
-  // Replace 'c' (but not 'ch') with 'k'
-  replaceCWithK(): void {
-    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace(/c(?!h)/, 'k');
+  // Replace 'qu' with 'kuw'
+  replaceQuWithKuw(): void {
+    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace('qu', 'kuw');
     this.updateInput();
-    this.processCurrentWord();  // Process again in case there are more 'c' occurrences
+    this.processCurrentWord();  // Process again in case there are more 'qu' occurrences
+  }
+    
+  // Replace 'qu' with 'k'
+  replaceQuWithK(): void {
+    this.words[this.currentWordIndex] = this.words[this.currentWordIndex].replace('qu', 'k');
+    this.updateInput();
+    this.processCurrentWord();  // Process again in case there are more 'qu' occurrences
   }
 
   // Move to the next word in the input
@@ -93,14 +139,24 @@ export class WordReviewDialogComponent {
     this.dialogRef.close(processedText);  // Return the final processed text
   }
 
+  promptForC(): void {
+    console.log(`Does the 'c' in '${this.words[this.currentWordIndex]}' sound like 's' or 'k'?`);
+    // Show dialog to get user input
+  }
+
   // Prompt methods (you need to implement the actual dialog for these)
   promptForCh(): void {
     console.log(`Does the 'ch' in '${this.words[this.currentWordIndex]}' sound like 'tiy' or 'k'?`);
     // Show dialog to get user input
   }
 
-  promptForC(): void {
-    console.log(`Does the 'c' in '${this.words[this.currentWordIndex]}' sound like 's' or 'k'?`);
+  promptForJ(): void {
+    console.log(`Does the 'j' in '${this.words[this.currentWordIndex]}' sound like 'diy' or 'h'?`);
+    // Show dialog to get user input
+  }
+
+  promptForQu(): void {
+    console.log(`Does the 'qu' in '${this.words[this.currentWordIndex]}' sound like 'kuw' or 'k'?`);
     // Show dialog to get user input
   }
 }
